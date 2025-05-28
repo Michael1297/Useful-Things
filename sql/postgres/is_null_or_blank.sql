ad380c521ac2bf5360bbdf02857c2e549704bc3d
Michael1297 <38764610+Michael1297@users.noreply.github.com>
2025-05-28 20:56:10 +0300

    Add files via upload

--- sql/postgres/is_null_or_blank.sql
+++ sql/postgres/is_null_or_blank.sql
@@ -1,35 +1,4 @@
 CREATE OR REPLACE FUNCTION is_null_or_blank(str TEXT)
 RETURNS BOOLEAN AS $$
-BEGIN
-    RETURN str IS NULL OR trim(str) = '';
-END;
-$$ LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE;
-
-
-COMMENT ON FUNCTION is_null_or_blank(text) IS
-'Проверяет, является ли строка NULL или пустой (состоит только из пробельных символов).
-
-Функция возвращает:
-  - TRUE, если строка равна NULL или состоит только из пробелов, табуляций и других пробельных символов.
-  - FALSE, если строка содержит хотя бы один непробельный символ.
-
-Параметры:
-  str (text): Строка для проверки.
-
-Возвращает:
-  boolean:
-    - TRUE, если строка NULL или пустая.
-    - FALSE, если строка содержит значимые символы.
-
-Примеры использования:
-  SELECT is_null_or_blank(NULL);
-  -- Результат: TRUE
-
-  SELECT is_null_or_blank(''   '');
-  -- Результат: TRUE
-
-  SELECT is_null_or_blank(''hello'');
-  -- Результат: FALSE
-
-Примечание:
-  Функция использует функцию trim() для удаления пробельных символов с обоих концов строки перед проверкой.';
+SELECT str IS NULL OR trim(str) = '';
+$$ LANGUAGE sql IMMUTABLE PARALLEL SAFE;
