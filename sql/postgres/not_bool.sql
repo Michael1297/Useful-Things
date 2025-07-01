c92dd7448f1192c0062695943ef691332c0b360b
Michael1297 <38764610+Michael1297@users.noreply.github.com>
2025-07-01 12:00:27 +0300

    Update not_bool.sql

--- sql/postgres/not_bool.sql
+++ sql/postgres/not_bool.sql
@@ -3,7 +3,6 @@ RETURNS boolean AS $$
 SELECT NOT $1;
 $$ LANGUAGE sql IMMUTABLE PARALLEL SAFE;
 
--- Добавляем комментарий к функции
 COMMENT ON FUNCTION public.not_bool(boolean) IS
 'Унарная функция, реализующая логическое НЕ (NOT).
 Используется для поддержки пользовательского оператора "!".
@@ -11,13 +10,11 @@ COMMENT ON FUNCTION public.not_bool(boolean) IS
 FALSE — если входное значение TRUE,
 NULL — если входное значение NULL.';
 
--- Создаем пользовательский префиксный оператор "!"
 CREATE OPERATOR public.! (
     PROCEDURE = public.not_bool,
     RIGHTARG = boolean
 );
 
--- Добавляем комментарий к оператору
 COMMENT ON OPERATOR public.! (NONE, boolean) IS
 'Пользовательский унарный оператор "!", эквивалентен ключевому слову NOT в SQL.
 Примеры:
